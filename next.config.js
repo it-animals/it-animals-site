46ec4a4ba3e61e7119b990f10907edf248dd5421
Kalinss <kalinss16@gmail.com>
2021-09-28 18:06:41 +0400

    upd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: process.env["base-path"],
-  assetPrefix: `${process.env["base-path"]}/`,
+  basePath: "/it-animals-site",
+  assetPrefix: `/it-animals-site/`,
   env,
 };
